58244eb8876dd731f065807f7e015e97d96419a7
yujung son <ths4030545@naver.com>
2020-09-14 19:27:08 +0900

    0914_6

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 AUTH_USER_MODEL = "users.User"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
